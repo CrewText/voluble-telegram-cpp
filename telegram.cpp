b8e5500ae2666fea966e5ed76244f5df056e64b4
calmcl1 <calmcl1@aol.com>
2019-04-30 08:42:51 +0100

    Initial commit

---
+++ telegram.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <iostream>
+#include <cpprest
+#include "VolubleTelegramClientConfig.h"
+
+using namespace std;
+
+int main()
+{
+    printf("Hi there!\n");
+    cout
+    << "VTH Version: "
+    << VolubleTelegramClient_VERSION_MAJOR << "." << VolubleTelegramClient_VERSION_MINOR 
+    << endl;
+    cout
+        << "hey" << endl;
+    return 0;
+}
